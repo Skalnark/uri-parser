d3fc0027646a3e74bbae38627fe7eb9f68a14418
Skalnark <darciobasilio@gmail.com>
2025-01-31 01:36:32 +0000

    Simple uri parser finished

--- app/Main.hs
+++ app/Main.hs
@@ -4,4 +4,6 @@ module Main (main) where
 import Parse
 
 main :: IO ()
-main = test "https://user:secret@example.com:8080/index.html#home"
+main = do
+    test "https://user:secret@example.com:8080/dir/index.html#home"
+    test "https://example.com/user/1234?email=email@example.com&age=99"
--- src/Parse.hs
+++ src/Parse.hs
@@ -40,12 +40,25 @@ pScheme = choice
 data Uri = Uri 
   { uriScheme :: Scheme
   , uriAuthority :: Maybe Authority
+  , path :: Maybe Path
   } deriving (Eq, Show)
 
 data Authority = Authority
   { authUser :: Maybe (Text, Text) -- user, password
   , authHost :: Text
   , authPort :: Maybe Int
+  } deriving (Eq, Show)
+
+data Path = Path
+  { pathSegments :: Text
+  , pathExtension :: Maybe Text
+  , pathQuery :: Maybe [PathVar]
+  , pathFragment :: Maybe Text
+  } deriving (Eq, Show)
+
+data PathVar = PathVar
+  { pathVarName :: Text
+  , pathVarValue :: Text
   } deriving (Eq, Show)
 
 pUri :: Parser Uri
@@ -63,4 +76,24 @@ pUri = do
     authHost <- T.pack <$> some (alphaNumChar <|> char '.') <?> "hostname"
     authPort <- optional (char ':' *> label "port number" L.decimal)
     return Authority {..}
-  return Uri {..}  
+  void (char '/')
+  path <- optional $ do
+    pathSegments <- T.pack <$> some (alphaNumChar <|> char '/') <?> "path"
+    pathExtension <- optional $ do
+      void (char '.')
+      T.pack <$> some alphaNumChar <?> "extension"
+    pathQuery <- optional $ do
+      void (char '?')
+      sepBy pathVar (char '&')
+    pathFragment <- optional $ do
+      void (char '#')
+      T.pack <$> some alphaNumChar <?> "fragment"
+    return Path {..}
+  return Uri {..}
+
+pathVar :: Parser PathVar
+pathVar = do
+  pathVarName <- T.pack <$> some alphaNumChar <?> "query variable name"
+  void (char '=')
+  pathVarValue <- T.pack <$> some (anySingleBut '&') <?> "query variable value"
+  return PathVar {..}
